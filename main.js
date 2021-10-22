e2dc1bdde61b3be6ab774c51358ead12e31892d4
Eric O'Brien <obrienjeric@gmail.com>
2021-10-21 20:11:07 -0400

    major overall

---
+++ main.js
@@ -0,0 +1,32 @@
+const parallax = document.getElementById('hero');
+
+window.addEventListener('scroll', function() {
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 1 + "px";
+    // console.log("Offset: " + offset); //logs the offset in the browser
+    // console.log("Offset * 0.7: " + offset * 0.7); //logs offset manipulated by the multiplier and gives us the factor we want for our parallax effect.
+});
+
+
+
+
+const contactInteractivity = () => {
+
+    const contactBtn = document.getElementById('contact_btn');
+    contactBtn.style.cursor = 'pointer';
+    contactBtn.addEventListener('mouseover', () => {
+        contactBtn.style.transition = '2s';
+        contactBtn.style.boxShadow = '3px 8px 8px grey';
+        contactBtn.style.opacity = '0.75';
+    })
+    contactBtn.addEventListener('mouseout', () => {
+        contactBtn.style.boxShadow = null;
+        contactBtn.style.opacity = 1;
+    })
+
+    contactBtn.addEventListener('click', () => {
+        window.open('https://www.linkedin.com/in/eric-o-brien44/');
+    })
+}
+
+contactInteractivity();
