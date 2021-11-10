4f5feafd9c2de444a4fcce7c57995bcbaf831d23
Eric O'Brien <obrienjeric@gmail.com>
2021-11-10 16:48:05 -0500

    adjusting contact section

--- main.js
+++ main.js
@@ -6,25 +6,3 @@ window.addEventListener('scroll', function() {
     // console.log("Offset: " + offset); //logs the offset in the browser
     // console.log("Offset * 0.7: " + offset * 0.7); //logs offset manipulated by the multiplier and gives us the factor we want for our parallax effect.
 });
-
-
-
-
-// const contactInteractivity = () => {
-
-//     const contactBtn = document.getElementById('contact_btn');
-//     contactBtn.style.cursor = 'pointer';
-//     contactBtn.addEventListener('mouseover', () => {
-//         contactBtn.style.opacity = '0.90';
-//     })
-//     contactBtn.addEventListener('mouseout', () => {
-//         contactBtn.style.boxShadow = null;
-//         contactBtn.style.opacity = 1;
-//     })
-//     contactBtn.addEventListener('click', () => {
-//         contactBtn.style.opacity = '0.75';
-//         window.open('https://www.linkedin.com/in/eric-o-brien44/');
-//     })
-// }
-
-// contactInteractivity();
